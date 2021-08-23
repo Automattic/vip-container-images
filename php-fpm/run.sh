b09d64508ec51c946e6dfd40cf7dae3c3c152a4c
Pavel Schoffer <xschof00@gmail.com>
2021-08-23 11:54:54 +0200

    Naming tweaks

--- php-fpm/run.sh
+++ php-fpm/run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-XDEBUG_CONFIG_LOCATION=/usr/local/php/docker-php-ext-xdebug.ini
+XDEBUG_CONFIG_TEMPLATE_LOCATION=/usr/local/php/docker-php-ext-xdebug.ini.template
 XDEBUG_CONFIG_TARGET_LOCATION=/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 
 if [ "enable" == "$XDEBUG" ]; then
     echo "Enabling XDebug"
 
-    cp $XDEBUG_CONFIG_LOCATION $XDEBUG_CONFIG_TARGET_LOCATION
+    cp $XDEBUG_CONFIG_TEMPLATE_LOCATION $XDEBUG_CONFIG_TARGET_LOCATION
 else
     echo "Disabling XDebug"
 
