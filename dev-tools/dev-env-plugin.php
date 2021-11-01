4c885e1b05ea18f64b11a79ec6dbf480d89eca82
jesse-greathouse <jesse.greathouse@gmail.com>
2021-11-01 15:04:52 -0600

    Explicitly defines which 2FA providers are able to be used in the dev-env.   * Rmoves the use of the problemic FIDO U2F 2FA provider while in the dev-env image

--- dev-tools/dev-env-plugin.php
+++ dev-tools/dev-env-plugin.php
@@ -8,3 +8,13 @@ add_filter( 'set_url_scheme', function( $url ) {
     }
     return $url;
 });
+
+// Limited Two-Factor Profiders for the dev-env
+add_filter('two_factor_providers', function() {
+    return array(
+        'Two_Factor_Email'        => TWO_FACTOR_DIR . 'providers/class-two-factor-email.php',
+        'Two_Factor_Totp'         => TWO_FACTOR_DIR . 'providers/class-two-factor-totp.php',
+        'Two_Factor_Backup_Codes' => TWO_FACTOR_DIR . 'providers/class-two-factor-backup-codes.php',
+        'Two_Factor_Dummy'        => TWO_FACTOR_DIR . 'providers/class-two-factor-dummy.php',
+    );
+});
