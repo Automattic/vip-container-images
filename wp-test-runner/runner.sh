0790d1cb65a72aa0272a86416136fc7af503000a
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2021-11-27 13:12:05 +0200

    Updates

--- wp-test-runner/runner.sh
+++ wp-test-runner/runner.sh
@@ -3,27 +3,27 @@
 set -e
 
 configure-environment
+create-database
 
 : "${PHP_OPTIONS:=""}"
 : "${PHPUNIT_VERSION:=""}"
 : "${APP_HOME:="/home/circleci/project"}"
+: "${PRETEST_SCRIPT:=""}"
 
 PHP="php ${PHP_OPTIONS}"
 
-if [ -f "${APP_HOME}/phpunit.xml" ] || [ -f "${APP_HOME}/phpunit.xml.dist" ]; then
-	if [ -x "${APP_HOME}/vendor/bin/phpunit" ] && [ -z "${PHPUNIT_VERSION}" ]; then
-		PHPUNIT="${APP_HOME}/vendor/bin/phpunit"
-	elif [ -n "${PHPUNIT_VERSION}" ] && [ -x "/usr/local/bin/phpunit${PHPUNIT_VERSION}" ]; then
-		PHPUNIT="/usr/local/bin/phpunit${PHPUNIT_VERSION}"
-	else
-		PHPUNIT=~/.composer/vendor/bin/phpunit
-	fi
-
-	echo "Running tests..."
-	# shellcheck disable=SC2086 # PHPUNIT_ARGS should not be quoted
-	${PHP} "${PHPUNIT}" ${PHPUNIT_ARGS} "$@"
+if [ -x "${APP_HOME}/vendor/bin/phpunit" ] && [ -z "${PHPUNIT_VERSION}" ]; then
+	PHPUNIT="${APP_HOME}/vendor/bin/phpunit"
+elif [ -n "${PHPUNIT_VERSION}" ] && [ -x "/usr/local/bin/phpunit${PHPUNIT_VERSION}" ]; then
+	PHPUNIT="/usr/local/bin/phpunit${PHPUNIT_VERSION}"
 else
-	echo "Unable to find phpunit.xml or phpunit.xml.dist in ${APP_HOME}"
-	ls -lha "${APP_HOME}"
-	exit 1
+	PHPUNIT=~/.composer/vendor/bin/phpunit
 fi
+
+if [ -n "${PRETEST_SCRIPT}" ] && [ -x "${PRETEST_SCRIPT}" ]; then
+	"${PRETEST_SCRIPT}"
+fi
+
+echo "Running tests..."
+# shellcheck disable=SC2086 # PHPUNIT_ARGS should not be quoted
+${PHP} "${PHPUNIT}" ${PHPUNIT_ARGS} "$@"
