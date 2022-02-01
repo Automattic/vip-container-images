c99257ecc7f5289a1500e411148c2a2966bb4ce3
jesse-greathouse <jessegreathouse@gmail.com>
2022-02-01 16:52:15 -0700

    * Changes to wordpress/versions.json to update the dev-environment images and metadata * On wordpress/add-version.sh I swapped the argument order so that the "tag" argument is the first argument * Fixed an error with the assignment of locked and prerelease arguments * Changed wordpress/add-version.sh so that it tries to match by tag instead of ref.   * The reason why I make this change is because I want to make an image of 5.7 that has a ref of 5.7.5, an image that already exists   * I want to make each line distinct by tag instead of ref * Changed wordpress/del-version.sh so that it deletes by tag instead of ref, to be consistent with the changes in wordpress/add-version.sh

--- wordpress/add-version.sh
+++ wordpress/add-version.sh
@@ -2,16 +2,16 @@
 
 if [ $# -lt 2 ]; then
     echo "Usage: add-version.sh <gitref> <tag> [cacheable]"
-    echo "  - <gitref>:     changeset/tag to import from the WordPress git repository"
     echo "  - <tag>:        tag for the resulting Docker image"
+    echo "  - <gitref>:     changeset/tag to import from the WordPress git repository"
     echo "  - <cacheable>:  optional parameter; if not empty, the ref will be marked as eligible for caching"
     echo "  - <locked>:     tag the image that it should be locked from automated curation"
     echo "  - <prerelease>: marks the image as using an unstable ref"
     exit 1
 fi
 
-REF="$1"
-TAG="$2"
+TAG="$1"
+REF="$2"
 
 if [ -n "$3" ]; then
     CACHEABLE=true
@@ -20,22 +20,22 @@ else
 fi
 
 if [ -n "$4" ]; then
-    LOCKED=false
-else
     LOCKED=true
+else
+    LOCKED=false
 fi
 
 if [ -n "$5" ]; then
-    PRERELEASE=false
-else
     PRERELEASE=true
+else
+    PRERELEASE=false
 fi
 
 VERSIONS="$(dirname "$0")/versions.json"
 
-exists=$(jq -r ".[] | select(.ref == \"${REF}\") | .ref" "${VERSIONS}")
+exists=$(jq -r ".[] | select(.tag == \"${TAG}\") | .ref" "${VERSIONS}")
 if [ -z "${exists}" ]; then
     jq ". += [{ref: \"${REF}\", tag: \"${TAG}\", cacheable: ${CACHEABLE}, locked: ${LOCKED}, prerelease: ${PRERELEASE} }] | sort" "${VERSIONS}" | sponge "${VERSIONS}"
 else
-    echo "${REF} already exists in versions.json"
+    echo "${TAG} already exists in versions.json"
 fi
