08c6c5f7e7e106a053c650230d87b206d0341158
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-10-12 00:55:35 +0300

    Use PHP 8.0 for core tests

--- wp-core-test-runner/configure-environment.sh
+++ wp-core-test-runner/configure-environment.sh
@@ -8,6 +8,8 @@ set -e
 : "${MYSQL_HOST="db"}"
 : "${WP_VERSION:="latest"}"
 
+sudo update-alternatives --set php "/usr/bin/php8.0"
+
 if [ ! -d "/wordpress/wordpress-core-${WP_VERSION}" ]; then
 	install-wp-core "${WP_VERSION}"
 fi
