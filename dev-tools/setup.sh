97aa9b448500038e16568089bf20a7d264251320
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-01-20 11:49:36 +0200

    feat(dev-tools): Add env var to enable ES

--- dev-tools/setup.sh
+++ dev-tools/setup.sh
@@ -59,7 +59,7 @@ fi
 echo "Copying dev-env-plugin.php to mu-plugins"
 cp /dev-tools/dev-env-plugin.php /wp/wp-content/mu-plugins/
 
-if [ -n "${LANDO_INFO}" ] && [ "$(echo "${LANDO_INFO}" | jq .elasticsearch.service)" != 'null' ]; then
+if [ -n "${ENABLE_ELASTICSEARCH}" ] || { [ -n "${LANDO_INFO}" ] && [ "$(echo "${LANDO_INFO}" | jq .elasticsearch.service)" != 'null' ]; }; then
   echo "Waiting for Elasticsearch to come online..."
   second=0
   while [ "$(curl -s http://elasticsearch:9200/_cluster/health | jq -r .status)" != 'green' ] && [ "${second}" -lt 60 ]; do
