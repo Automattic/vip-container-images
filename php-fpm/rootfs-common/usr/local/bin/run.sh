facd1597ac12b78f38c3fe0185790af5a68e7657
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-04-10 13:13:36 +0300

    feat(php-fpm): Add support for mailpit

--- php-fpm/rootfs-common/usr/local/bin/run.sh
+++ php-fpm/rootfs-common/usr/local/bin/run.sh
@@ -13,10 +13,13 @@ fi
 
 if [ -n "${LANDO_INFO}" ] && [ 'null' != "$(echo "${LANDO_INFO}" | jq -r .mailhog)" ]; then
     echo "sendmail_path = /usr/sbin/sendmail -S $(echo "${LANDO_INFO}" | jq -r '.mailhog.internal_connection.host + ":" + .mailhog.internal_connection.port')" > "${PHP_INI_DIR}/conf.d/99-mailhog.ini"
+elif [ -n "${LANDO_INFO}" ] && [ 'null' != "$(echo "${LANDO_INFO}" | jq -r .mailpit)" ]; then
+    echo "sendmail_path = /usr/sbin/sendmail -S mailpit:1025" > "${PHP_INI_DIR}/conf.d/99-mailpit.ini"
 elif [ -n "${ENABLE_MAILHOG}" ]; then
     echo "sendmail_path = /usr/sbin/sendmail -S mailhog:1025" > "${PHP_INI_DIR}/conf.d/99-mailhog.ini"
 else
     rm -f "${PHP_INI_DIR}/conf.d/99-mailhog.ini"
+    rm -f "${PHP_INI_DIR}/conf.d/99-mailpit.ini"
 fi
 
 exec /usr/sbin/php-fpm
