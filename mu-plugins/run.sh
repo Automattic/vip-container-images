e09673917b5f79ea5a9dc77de8978f4c91466d73
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-12-07 17:10:52 +0200

    refactor(mu-plugins): let entrypoint script terminate

--- mu-plugins/run.sh
+++ mu-plugins/run.sh
@@ -2,19 +2,6 @@
 
 if [ ! -f /shared/.version ] || ! cmp -s /shared/.version /mu-plugins/.version; then
     rm -f /shared/.version
-    if getent passwd www-data; then
-        CHOWN=--chown=www-data:www-data
-    else
-        CHOWN=
-    fi
-
-    rsync -a --delete-after --exclude='/.version' ${CHOWN} /mu-plugins/ /shared/
-
-    if [ -n "${CHOWN}" ]; then
-        install -m 0644 -o www-data -g www-data /mu-plugins/.version /shared/
-    else
-        cp /mu-plugins/.version /shared/
-    fi
+    rsync -a --delete-after --exclude='/.version' /mu-plugins/ /shared/
+    cp /mu-plugins/.version /shared/
 fi
-
-exec /bin/sleep infinity
