166253d3b079f5dd332c964edeab3a243478a235
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2025-05-30 18:51:13 +0300

    feat: more colors

--- wordpress/dev-tools/backfill-env-vars.php
+++ wordpress/dev-tools/backfill-env-vars.php
@@ -1,6 +1,20 @@
 <?php
 $env_file = '/app/.env';
 
+if ( stream_isatty( STDERR ) ) {
+    $strong = '\x1B[1m';
+    $red    = '\x1B[31m';
+    $green  = '\x1B[32m';
+    $yellow = '\x1B[33m';
+    $reset  = '\x1B(B\x1B[m';
+} else {
+    $strong = '';
+    $red    = '';
+    $green  = '';
+    $yellow = '';
+    $reset  = '';
+}
+
 if ( is_file( $env_file ) && is_readable( $env_file ) && is_writable( $env_file ) && is_writable( dirname( $env_file ) ) && is_readable( '/wp/wp-load.php' ) ) {
     require_once '/wp/wp-load.php';
 
@@ -11,6 +25,8 @@ if ( is_file( $env_file ) && is_readable( $env_file ) && is_writable( $env_file 
             list( $key, $value ) = explode( '=', $line, 2 );
             $key   = trim( $key );
             $value = trim( $value );
+            // Double quotes: special characters are ", $, and \. They are escaped with a slash
+            // Single quotes: special character is '. It is escaped with a slash
             if ( str_starts_with( $value, '"' ) && str_ends_with( $value, '"' ) ) {
                 $value = substr( $value, 1, -1 );
                 $value = str_replace( [ '\\"', '\\$', '\\\\' ], [ '"', '$', '\\' ], $value );
@@ -26,7 +42,7 @@ if ( is_file( $env_file ) && is_readable( $env_file ) && is_writable( $env_file 
     $existing_vars = array_filter( get_defined_constants(), fn ( $key ) => str_starts_with( $key, 'VIP_ENV_VAR_' ), ARRAY_FILTER_USE_KEY );
     foreach ( $existing_vars as $key => $value ) {
         if ( ! empty( $env_vars[ $key ] ) && $env_vars[ $key ] !== $value ) {
-            fprintf( STDERR, 'WARNING: Environment variable "%s" already exists with a different value ("%s" vs "%s"). Ignoring.\n', $key, $env_vars[ $key ], $value );
+            fprintf( STDERR, "{$yellow}{$strong}WARNING:{$reset} Environment variable \"%s\" already exists with a different value (\"%s\" vs \"%s\"). Ignoring.\n", $key, $env_vars[ $key ], $value );
         } else {
             $env_vars[ $key ] = $value;
         }
@@ -43,12 +59,12 @@ if ( is_file( $env_file ) && is_readable( $env_file ) && is_writable( $env_file 
     }
 
     if ( file_put_contents( $env_file . '.tmp', $env ) === false ) {
-        fprintf( STDERR, "ERROR: Failed to write to %s\n", $env_file );
+        fprintf( STDERR, "{$red}{$strong}ERROR:{$reset} Failed to write to %s\n", $env_file );
     } else {
         if ( ! rename( $env_file . '.tmp', $env_file ) ) {
-            fprintf( STDERR, "ERROR: Failed to rename temporary file to %s\n", $env_file );
+            fprintf( STDERR, "{$red}{$strong}ERROR:{$reset} Failed to rename temporary file to %s\n", $env_file );
         } else {
-            printf( "Environment variables have been successfully updated.\n" );
+            printf( "{$green}Environment variables have been successfully updated.{$reset}\n" );
         }
     }
 }
