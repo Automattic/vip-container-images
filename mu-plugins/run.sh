5b90040d4f8d5813fd5b14a9cb158f3f4e068c75
Rinat K <rinat.khaziev@automattic.com>
2025-06-25 11:42:31 -0500

    Fix mu-plugins to copy files even if the size is the same (#1188)
    * Fix mu-plugins to copy files even if the size is the same

--- mu-plugins/run.sh
+++ mu-plugins/run.sh
@@ -2,6 +2,6 @@
 
 if [ ! -f /shared/.version ] || ! cmp -s /shared/.version /mu-plugins/.version; then
     rm -f /shared/.version
-    rsync -a --delete-after --exclude='/.version' /mu-plugins/ /shared/
+    rsync -ac --delete-after --exclude='/.version' /mu-plugins/ /shared/
     cp /mu-plugins/.version /shared/
 fi
