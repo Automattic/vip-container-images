1ba9c3b56cb1d6954e4587502312a9848e90e24f
Sean Langlands <sean.langlands@automattic.com>
2026-05-25 10:34:28 -0700

    fix(photon): handle subdir network site URLs in local fetch filter

--- photon/rootfs/etc/photon/config.php
+++ photon/rootfs/etc/photon/config.php
@@ -10,20 +10,40 @@ define( 'JPEGTRAN', '/usr/bin/jpegtran' );
 if ( function_exists( 'add_filter' ) ) {
 	add_filter( 'override_raw_data_fetch', function ( $_overridden_data, $url ) {
 		global $remote_image_max_size;
-		if ( preg_match( '!^https?://wp-content/uploads/!', $url ) && ! empty( $_SERVER['DOCUMENT_ROOT'] ) ) {
-			$path = realpath( preg_replace( '!^https?://wp-content!', $_SERVER['DOCUMENT_ROOT'], $url ) );
-			if ( str_starts_with( $path, $_SERVER['DOCUMENT_ROOT'] ) ) {
-				if ( isset( $remote_image_max_size ) && $remote_image_max_size > 0 ) {
-					$size = filesize( $path );
-					if ( $size > $remote_image_max_size ) {
-						return false;
-					}
-				}
 
-				return file_get_contents( $path );
+		if ( empty( $_SERVER['DOCUMENT_ROOT'] ) ) {
+			return $_overridden_data;
+		}
+
+		/*
+		 * Photon builds a synthetic URL from REQUEST_URI (path is used as the "host"):
+		 * Example: https://wp-content/uploads/sites/2/2026/05/image.jpg (subdomain)
+		 * Example: https://test/wp-content/uploads/sites/2/2026/05/image.jpg (subdirectory blog at /test/)
+		 * Map to DOCUMENT_ROOT/uploads/... (wp-content/uploads is mounted at photon/uploads).
+		 */
+		if ( ! preg_match( '!/wp-content/uploads/([^?]+)!', $url, $matches ) ) {
+			return $_overridden_data;
+		}
+
+		$document_root = realpath( $_SERVER['DOCUMENT_ROOT'] );
+
+		if ( false === $document_root ) {
+			return $_overridden_data;
+		}
+
+		$path = realpath( $document_root . '/uploads/' . $matches[1] );
+		
+		if ( false === $path || ! str_starts_with( $path, $document_root ) ) {
+			return $_overridden_data;
+		}
+
+		if ( isset( $remote_image_max_size ) && $remote_image_max_size > 0 ) {
+			$size = filesize( $path );
+			if ( $size > $remote_image_max_size ) {
+				return false;
 			}
 		}
 
-		return false;
+		return file_get_contents( $path );
 	}, 10, 2 );
 }
